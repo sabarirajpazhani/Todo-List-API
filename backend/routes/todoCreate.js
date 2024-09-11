62d91ee22902a0d145af23f69d994a4e2721edd8
sabarirajpazhani <sabarirajpazhani@gmail.com>
2024-09-11 21:07:07 +0530

    update

--- backend/routes/todoCreate.js
+++ backend/routes/todoCreate.js
@@ -1,4 +1,7 @@
-const express = require("express")
+const express = require("express");
+const { todoCreating } = require("../controllers/todoCreateController");
 const router = express.Router();
 
-router.route('/').post();
+router.route('/todo').post(todoCreating);
+
+module.exports=router;

3301b183ec1a9c8674fb5331cf69db4001a8135d
sabarirajpazhani <sabarirajpazhani@gmail.com>
2024-09-11 22:47:51 +0530

    Create Todo API

--- backend/controllers/todoCreateController.js
+++ backend/controllers/todoCreateController.js
@@ -1,6 +1,31 @@
-exports.todoCreating=(req, res, next)=>{
-    res.json({
-        success:true,
-        message:"Creation is working"
-    })
+const todoModel = require("../models/todoModels");
+
+exports.todoCreating=async(req, res, next)=>{
+    try{
+        const {task} = req.body;
+    
+        if(!task){
+            return res.status(400).json({
+                message:"Task field is required"
+            })
+        }
+
+        let tasks = await todoModel.findOne({task});
+        if(tasks){
+            return res.status(409).json({
+                message:"Taks already Exists"
+            })
+        }
+        tasks = new todoModel({task});
+        await tasks.save();
+
+        res.status(200).json({
+            message:"New Todo Created Successfully"
+        })
+    }
+    catch(err){
+        res.status(500).json({
+            message:"Internal Server Error"
+        })
+    }
 }

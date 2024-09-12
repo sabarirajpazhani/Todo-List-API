97b9a8018e8d01b29c5810e28c6aa6c77b0d2e7b
sabarirajpazhani <sabarirajpazhani@gmail.com>
2024-09-12 23:20:04 +0530

    Update API

--- backend/controllers/todoUpdateController.js
+++ backend/controllers/todoUpdateController.js
@@ -3,12 +3,12 @@ const todoModel = require("../models/todoModels");
 exports.todoUpdating = async(req,res,next)=>{
     try{
         const task = req.body.task;
-        const completed = req.body.conpleted;
+        const completed = req.body.completed;
 
         const todo = await todoModel.findById(req.params.id);
         if(!todo){
             return res.status(404).json({
-                message:"Todo nor found"
+                message:"Todo not found"
             })
         }
 

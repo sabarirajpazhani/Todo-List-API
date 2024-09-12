92a540388734a2adc2230ec1026b9409852a835c
sabarirajpazhani <sabarirajpazhani@gmail.com>
2024-09-12 23:27:28 +0530

    Delete todo API

--- backend/controllers/todoDeleteController.js
+++ backend/controllers/todoDeleteController.js
@@ -1,6 +1,22 @@
-exports.todoDeleting =(req,res,next)=>{
-    res.json({
-        success:true,
-        message:"Deleting is working"
-    })
+const todoModel = require("../models/todoModels")
+
+exports.todoDeleting =async(req,res,next)=>{
+    try{
+        const todo = await todoModel.findById(req.params.id);
+        if(!todo){
+            return res.status(404).json({
+                message:"Todo Not Found"
+            })
+        }
+
+        await todo.remove();
+        res.json({
+            message:"Todo Deleted Successfully"
+        })
+    }
+    catch(err){
+        res.status(500).json({
+            message:"Internal Server error"
+        })
+    }
 }
